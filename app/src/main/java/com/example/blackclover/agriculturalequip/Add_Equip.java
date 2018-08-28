4dc0ba1a0434679e37a0ca79e69e51b96dc58212
BlackClover <first.cmu.se@gmail.com>
2018-08-28 18:01:54 +0700

    Fix add equipment.

--- app/src/main/java/com/example/blackclover/agriculturalequip/Add_Equip.java
+++ app/src/main/java/com/example/blackclover/agriculturalequip/Add_Equip.java
@@ -6,33 +6,46 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.blackclover.agriculturalequip.Model.Equipment;
 
 public class Add_Equip extends AppCompatActivity {
 
     Button btnAdd;
+
+    EditText edt_name;
+
     CheckBox cbStarter,cbFuelTank,cbAirFilter,cbCarburetor,cbCylinder,cbBallValve,cbMuffler,cbSwitchOnOff,cbCoil,cbFuelTankCap,cbOilTankCap,cbSparkPlug,cbControlSwitch,cbBrushCutterBlade,cbGearDiver,cbMainPipe,cbShaft,cbAirChamber,cbAdjustSet,cbDischargeMetal,cbSuctionMetal,cbPistonSet,
     cbStarterRopeReel,cbPressureGauge,cbPaint;
     Equipment objEquip;
 
+    String equip_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add__equip);
+        setContentView(R.layout.activity_add_equip);
 
         bindWidget();
-
+        
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentMainMenu = new Intent(getApplicationContext(), EstinateActivity.class);
-                startActivity(intentMainMenu);
+                equip_name = String.valueOf(edt_name.getText());
+
+                Toast.makeText(Add_Equip.this, "Add " + equip_name + " success.", Toast.LENGTH_SHORT).show();
+                Intent intentToMain = new Intent(getApplicationContext(), MainActivity.class);
+                //startActivity(intentToMain);
             }
         });
+
+
     }
 
     private void bindWidget() {
-
+        btnAdd = findViewById(R.id.btnAdd);
+        edt_name = findViewById(R.id.txt_name);
     }
 }
