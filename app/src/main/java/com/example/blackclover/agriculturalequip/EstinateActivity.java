f9d6b2ac2292992da1407ca71082890f6fdfbabf
BlackClover <first.cmu.se@gmail.com>
2018-08-28 17:44:14 +0700

    Add spinner.

--- app/src/main/java/com/example/blackclover/agriculturalequip/EstinateActivity.java
+++ app/src/main/java/com/example/blackclover/agriculturalequip/EstinateActivity.java
@@ -2,21 +2,39 @@ package com.example.blackclover.agriculturalequip;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class EstinateActivity extends AppCompatActivity {
+
+    private Spinner spn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estinate);
 
-        Spinner spn = findViewById(R.id.spinner_list_equip);
+        spn = findViewById(R.id.spinner_list_equip);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array,
                 android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spn.setAdapter(adapter);
+
+        //Set on item selected.
+        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(EstinateActivity.this, "Select : " + spn.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
