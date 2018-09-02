89f10e49844b82e7961e1701d34e35ea211d7f18
iFirst <first.cmu.se@gmail.com>
2018-09-02 21:41:52 +0700

    Apply Room Persistence Library. Create fragment.

--- app/src/main/java/com/example/blackclover/agriculturalequip/Database/Table_Equipment.java
+++
@@ -1,9 +0,0 @@
-package com.example.blackclover.agriculturalequip.Database;
-
-/**
- * Created by BlackClover on 8/28/2018.
- */
-
-public class Table_Equipment {
-
-}
---
+++ app/src/main/java/com/example/blackclover/agriculturalequip/Entity/Purchase.java
@@ -0,0 +1,4 @@
+package com.example.blackclover.agriculturalequip.Entity;
+
+public class Purchase {
+}
